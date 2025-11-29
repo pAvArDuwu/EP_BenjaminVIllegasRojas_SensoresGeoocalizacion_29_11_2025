927dc9bcf5105bb9f0d58444dd3f2094521bf75c
MiAplicacion <benjaminvillegasrojas@gmail.com>
2025-11-29 16:29:44 -0400

    Subiendo cambios actualizados

--- app/src/main/java/com/example/ep_benjaminvillegasrojas_sesoresgreolocalizacion_29112025/MainActivity.java
+++ app/src/main/java/com/example/ep_benjaminvillegasrojas_sesoresgreolocalizacion_29112025/MainActivity.java
@@ -1,24 +1,107 @@
 package com.example.ep_benjaminvillegasrojas_sesoresgreolocalizacion_29112025;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, OnMapReadyCallback {
+
+    private SensorManager sensorManager;
+    private Sensor acelerometro;
+    private TextView textViewX, textViewY, textViewZ;
+
+    private GoogleMap mMap;
+    private Marker marker;
+    private static final LatLng SANTA_CRUZ = new LatLng(-17.7833, -63.1833);
+    private static final float SHAKE_THRESHOLD = 2.0f;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        textViewX = findViewById(R.id.textViewX);
+        textViewY = findViewById(R.id.textViewY);
+        textViewZ = findViewById(R.id.textViewZ);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager != null) {
+            acelerometro = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        }
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            long curTime = System.currentTimeMillis();
+
+            if ((curTime - lastUpdate) > 100) {
+                long diffTime = (curTime - lastUpdate);
+                lastUpdate = curTime;
+
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+
+                textViewX.setText("X: " + x);
+                textViewY.setText("Y: " + y);
+                textViewZ.setText("Z: " + z);
+
+                float velocidad = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+
+                last_x = x;
+                last_y = y;
+                last_z = z;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        marker = mMap.addMarker(new MarkerOptions().position(SANTA_CRUZ).title("Santa Cruz"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SANTA_CRUZ, 15));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (acelerometro != null) {
+            sensorManager.registerListener(this, acelerometro, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
